ba4e5723884057f50a0474272b13cbdaf42aefa1
Satyam <satyamismyname@gmail.com>
2019-02-24 11:22:29 +0530

    uptil DetailsUI_Hack ...adding details of event 2

---
+++ app/src/main/java/com/creation/android/lafetegita/DetailsEssayOnline.java
@@ -0,0 +1,23 @@
+package com.creation.android.lafetegita;
+
+import android.support.design.widget.NavigationView;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.FrameLayout;
+
+public class DetailsEssayOnline extends BaseActivity {
+
+    Button reg_btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        FrameLayout contentFrameLayout = (FrameLayout) findViewById(R.id.content_frame); //Remember this is the FrameLayout area within your activity_main.xml
+        getLayoutInflater().inflate(R.layout.activity_details_essay_online, contentFrameLayout);
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.getMenu().getItem(1).setChecked(true);
+
+        reg_btn = findViewById(R.id.reg_btn_id);
+
+    }
+}
